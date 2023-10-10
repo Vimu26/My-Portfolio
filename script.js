f737754d6043bea6f52f27adae726ce7c2894884
akalanka <akalanka@echonlabs.com>
2023-10-10 10:54:06 +0530

    fixes

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-let menuIcon = document.querySelector('.menu-icon');
+let menuIcon = document.querySelector(".menu-icon");
 
-menuIcon.addEventListener('click', ()=>{
-    menuIcon.classList.toggle('active');
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("active");
 });
 
 let text = document.querySelector(".text p");
 
-text.innerHTML = text.innerHTML.split("").map((char,i)=>
-    `<b style="transform: rotate(${i * 6.3}deg)">${char}</b>`
-).join("");
+text.innerHTML = text.innerHTML
+  .split("")
+  .map((char, i) => `<b style="transform: rotate(${i * 6.3}deg)">${char}</b>`)
+  .join("");
 
 const buttons = document.querySelectorAll(".about-btn button");
 const contents = document.querySelectorAll(".content");
 
-buttons.forEach((button , index )=>{
-    button.addEventListener( "click",()=>{
-         contents.forEach(content=> content.style.display = "none");
-         contents[index].style.display = "block";
-       buttons.forEach(btn => btn.classList.remove("active"));
-       button.classList.add("active");
-    
-    });
-   
-})
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    contents.forEach((content) => (content.style.display = "none"));
+    contents[index].style.display = "block";
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+  });
+});
