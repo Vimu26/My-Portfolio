289a0d6a5fa68bacbc85c796b0ed98eb90fab36b
akalanka <akalanka@echonlabs.com>
2023-10-14 12:28:28 +0530

    skill chart done

--- script.js
+++ script.js
@@ -22,3 +22,59 @@ buttons.forEach((button, index) => {
     button.classList.add("active");
   });
 });
+
+const skill1 = document.getElementById("number1");
+let counter1 = 0 ;
+const html = "HTML"
+setInterval(() => {
+    if(counter1 == 85){
+        clearInterval();
+    }
+    else {
+          counter1 += 1;
+number1.innerHTML = counter1 + "%" + `<br><h4>${html}</h4>` ;
+    }
+  
+},22)
+
+const skill2 = document.getElementById("number2");
+let counter2 = 0 ;
+const css = "CSS"
+setInterval(() => {
+    if(counter2 == 85){
+        clearInterval();
+    }
+    else {
+          counter2 += 1;
+number2.innerHTML = counter2 + "%" + `<br><h4>${css}</h4>` ;
+    }
+  
+},22)
+
+const skill3 = document.getElementById("number3");
+let counter3 = 0 ;
+const js = "Javascript"
+setInterval(() => {
+    if(counter3 == 85){
+        clearInterval();
+    }
+    else {
+          counter3 += 1;
+number3.innerHTML = counter3 + "%" + `<br><h4>${js}</h4>` ;
+    }
+  
+},22)
+
+const skill4 = document.getElementById("number2");
+let counter4 = 0 ;
+const ts = "TypeScript"
+setInterval(() => {
+    if(counter2 == 85){
+        clearInterval();
+    }
+    else {
+          counter4 += 1;
+number4.innerHTML = counter4 + "%" + `<br><h4>${ts}</h4>` ;
+    }
+  
+},22)
